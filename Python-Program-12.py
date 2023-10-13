5ab8e7d63cc69cbcf1129dcd9e434a3d54bf9bb7
Rohit Shah <132545484+ROHIT-0P@users.noreply.github.com>
2023-10-13 11:21:36 +0530

    Update Python-Program-12.py

--- Python-Program-12.py
+++ Python-Program-12.py
@@ -7,3 +7,33 @@ b = 50
 result = add(a, b)
 
 print("Addition of ", a, " and ", b, " is = ", result)
+
+def sub(a,b):
+    return (a-b)
+    
+a = 90
+b = 50
+
+result = sub(a, b)
+
+print("Subtraction of ", a, " and ", b, " is = ", result)
+
+def mul(a,b):
+    return (a*b)
+    
+a = 90
+b = 50
+
+result = mul(a, b)
+
+print("Multiplication of ", a, " and ", b, " is = ", result)
+
+def div(a,b):
+    return (a/b)
+    
+a = 90
+b = 50
+
+result = div(a, b)
+
+print("Division of ", a, " and ", b, " is = ", result)
