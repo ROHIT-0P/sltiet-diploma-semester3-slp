95e4b2a54f6688dd34fa12c408c253994b324f71
Rohit Shah <132545484+ROHIT-0P@users.noreply.github.com>
2023-10-16 12:27:57 +0530

    Create Python-Program-14.py

---
+++ Python-Program-14.py
@@ -0,0 +1,10 @@
+#Importing datetime from datetime module
+from datetime import datetime
+
+#Assigning datetime to today variable
+today = datetime.now()
+
+#Printing Current Year,Month & Day using today variable
+print("Current Year",today.year)
+print("Current Month",today.month)
+print("Current Day",today.day)
