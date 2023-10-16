53dd6d892a59eb435229a2e17cea0bbc59327853
Rohit Shah <132545484+ROHIT-0P@users.noreply.github.com>
2023-10-16 12:32:34 +0530

    Create Python-Program-15.py

---
+++ Python-Program-15.py
@@ -0,0 +1,14 @@
+#Importing datetime from datetime module
+from datetime import datetime
+
+#Assigning date using datetime() in a variable
+a = datetime(2018,11,28)
+
+#Printing value of a
+print(a)
+
+#Assigning date & time using datetime() in b variable
+b = datetime(2023,11,28,23,55,59,343280)
+
+#Printing value of b
+print(b)
